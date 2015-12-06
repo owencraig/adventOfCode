75c08889ad389644118d06fb4b06fdeb896b9a8f
Owen Craig <owen@owencraig.com>
2015-12-06 12:13:30 -0600

    refactored day1/part2 to remove multi line function

--- day1/part2.js
+++ day1/part2.js
@@ -1,13 +1,11 @@
 var part1 = require('./part1');
-function mapWithResult(cb){
-	return this.reduce((result,current,index) => {
-		result[index] = cb(current, index, result);
-		return result;
-	}, []);
-}
 
-var sumAlong = (current, index, arr) => part1.sum(current, arr[index-1] || 0);
-var getFirstTimeAt = (level, input) => mapWithResult.call(part1.getSteps(input), sumAlong).indexOf(level) + 1;
+var pushAndReturnArray = (array, value) => array.push(value) && array; // since push will always return at least 1 for the length
+var reduceToArray = (transform, result,current, index, array) => pushAndReturnArray(result, transform(current, index, array, result));
+var mapWithResult = (input, transform) => input.reduce(reduceToArray.bind(null, transform), []);
+var sumAlong = (current, index, _, arr) => part1.sum(current, arr[index-1] || 0);
+var getFirstTimeAt = (level, input) => mapWithResult(part1.getSteps(input), sumAlong).indexOf(level) + 1;
 var getFirstTimeAtBasement = getFirstTimeAt.bind(null, -1);
+
 module.exports = getFirstTimeAtBasement;
 
